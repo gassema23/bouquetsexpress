d9f739f2f0ca211451aa487157dabf900b98849d
gassema23 <gasse.mathieu@gmail.com>
2023-11-21 20:32:53 -0500

    page a propos mobile rdy

--- src/pages/APropos.jsx
+++ src/pages/APropos.jsx
@@ -23,26 +23,27 @@ const APropos = () => {
                     </div>
                 </div>
             </Hero>
-            <div className='flex w-full container mx-auto py-10 flex-col gap-4'>
-                <div className='flex w-full pb-4 justify-center items-center'>
-                    <div className=' w-full '>
+            <div className='container mx-auto px-4 md:px-0'>
+
+                <div className='flex w-full py-10 md:justify-center md:items-center flex-col md:flex-row'>
+                    <div>
                         <h2 className='font-bold text-4xl pb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <p>Vestibulum dignissim justo sed sapien ullamcorper, a dictum massa posuere. Integer nisi elit, congue id efficitur rutrum, suscipit nec elit. Donec mattis sed ligula in commodo. Vivamus viverra lectus id dui ultricies tempus. Morbi a eleifend orci. Vivamus nibh erat, mattis sed ullamcorper in, bibendum eu mauris. Donec eget facilisis diam, eget consectetur ex. Pellentesque vitae risus interdum, facilisis velit ut, finibus ipsum. Morbi ullamcorper feugiat purus vel sodales.</p>
                     </div>
-                    <div className='w-full'>
-                        <img src="/images/will-TWJmj182gu4-unsplash.jpg" alt="" className='h-auto max-w-lg mx-auto' />
+                    <div className='md:pt-0 pt-4'>
+                        <img src="/images/will-TWJmj182gu4-unsplash.jpg" alt="" className='h-auto md:max-w-lg md:mx-auto' />
                     </div>
                 </div>
 
-                <div className='flex flex-col w-full text-center pt-4'>
+                <div className='flex flex-col w-full text-center pt-10'>
                     <h5 className='font-bold text-xl pb-2'>Ut et finibus ante</h5>
                     <h3 className='font-bold text-3xl pb-4'>Notre Équipe</h3>
                 </div>
-                <div className='grid grid-cols-4 gap-4'>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-4 pb-10'>
                     <TeamCard nom="Karen Nelson" image="/images/members/member-10.png" description="Finance" />
                     <TeamCard nom="Helene Engels" image="/images/members/member-4.png" description="Co-Fondateur" />
                     <TeamCard nom="Leslie Libingston" image="/images/members/member-2.png" description="Co-Fondateur" />
-                    <TeamCard nom="Joseph McFall" image="/images/members/member-3.png" description="Technicien" />
+                    <TeamCard nom="Joseph McFall" image="/images/members/member-3.png" description="Technicien Fleuriste" />
                 </div>
             </div>
         </div>
